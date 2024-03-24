4940aaeb74237d0c5802bacf61c26240ca7d8a40
Jan-Kristian Karlsen <jan-kristian@jankristiansair.lan>
2024-03-24 22:26:18 +0100

    Added objects to the array, created a book div and its child elements, including title, author and genre. Loops over each element in array and adds to DOM

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-const myLibrary = ["Harry Potter","LOTR","Game of Thrones"];
+const myLibrary = [{Title:"Harry Potter", Author:"J.K. Rowling",Genre:"Fantasy"},{Title: "LOTR", Author:"J.R.R. Tolkien", Genre: "Fantasy" },{Title: "Game of Thrones", Author: "George R.R. Martin", Genre: "Fantasy"}];
+const bookcase = document.querySelector(".bookcase");
+
+
 
 function displayBooks() {
     myLibrary.forEach(book => {
-        
+        // Create elements that go into the bookcase
+        let createdDiv = document.createElement("div");
+        let createdTitle = document.createElement("h2");
+        let createdText_1 = document.createElement("p");
+        let createdText_2 = document.createElement("p");
+
+        // add text to elements
+        createdTitle.textContent = "Title: " + book.Title;
+        createdText_1.textContent = "Author: " + book.Author;
+        createdText_2.textContent = "Genre: " + book.Genre;
+
+
+        // append elements to div
+        createdDiv.appendChild(createdTitle);
+        createdDiv.appendChild(createdText_1);
+        createdDiv.appendChild(createdText_2);
+
+        createdDiv.classList.add("book");
+        bookcase.appendChild(createdDiv);
     });
 }
 
@@ -13,3 +34,5 @@ function Book() {
 function addBookToLibrary() {
   // do stuff here
 }
+
+displayBooks();
