38f153eacd8b7c9b7147d0ea2468f9fa7ee17c71
Jan-Kristian Karlsen <jan-kristian@jankristiansair.lan>
2024-03-26 14:17:01 +0100

    can successfully now add books using the form in the website. Each time a new class of Book is created and added to the array and then displayed on the site. next is to create a remove button on each book followed by a method of reading the book.

--- script.js
+++ script.js
@@ -1,8 +1,59 @@
-const myLibrary = [{Title:"Harry Potter", Author:"J.K. Rowling",Genre:"Fantasy",HasRead:true},{Title: "LOTR", Author:"J.R.R. Tolkien", Genre: "Fantasy", HasRead:false },{Title: "Game of Thrones", Author: "George R.R. Martin", Genre: "Fantasy", HasRead:false}];
+class Book {
+        // the constructor...
+    constructor(title, author, genre, hasRead) {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.hasRead = hasRead;
+    }
+
+    get title() {
+        return this._title;
+    }
+
+    set title(title) {
+        this._title = title;
+    }
+
+    get hasRead() {
+        return this._hasRead;
+    }
+
+    set hasRead(hasRead) {
+        if(hasRead) {
+            this._hasRead = false;
+        } else {
+            true;
+        }
+    }
+    
+    info() {
+        return `${this.title} by ${this.author}, genre: ${this.genre}, has read: ${this.hasRead}`;
+    }
+}
+
+const myLibrary = [new Book("Harry Potter", "J.K. Rowling","Fantasy",true)];
 const bookcase = document.querySelector(".bookcase");
 const addBookButton = document.querySelector(".add-book");
 const button = document.querySelector(".add-book-button");
 const form = document.querySelector(".add-book-form");
+const submitBook = document.querySelector(".submit-book-button");
+
+submitBook.addEventListener('click', function(event) {
+    event.preventDefault();
+    console.log("Submit button is clicked");
+    addBookToLibrary();
+    // Prevent form submission
+});
+
+
+button.addEventListener('click', function(event) {
+    event.preventDefault();
+    console.log("Form is clicked");
+    showForm();
+     // Prevent form submission
+    
+});
 
 
 
@@ -10,10 +61,13 @@ function showForm() {
     if (form.style.display === "block") {
         form.style.display = "none";
         console.log("Form is hidden");
+        button.textContent = "Add book";
         return;
     } else {
         form.style.display = "block";
+        button.textContent = "Cancel";
         console.log("Form is visible");
+        return
     }
     
 }
@@ -30,10 +84,10 @@ function displayBooks() {
         let createdText_3 = document.createElement("p");
 
         // add text to elements
-        createdTitle.textContent = "Title: " + book.Title;
-        createdText_1.textContent = "Author: " + book.Author;
-        createdText_2.textContent = "Genre: " + book.Genre;
-        createdText_3.textContent = "Has Read: " + book.HasRead;
+        createdTitle.textContent = "Title: " + book.title;
+        createdText_1.textContent = "Author: " + book.author;
+        createdText_2.textContent = "Genre: " + book.genre;
+        createdText_3.textContent = "Has Read: " + book.hasRead;
 
 
         // append elements to div
@@ -47,33 +101,21 @@ function displayBooks() {
     });
 }
 
-class Book {
-    constructor() {
-        // the constructor...
-        constructor(title, author, genre, hasRead); {
-            this.title = title;
-            this.author = author;
-            this.genre = genre;
-            this.hasRead = hasRead;
-        };
-    }
 
-    
-    
-    info() {
-        return `${this.title} by ${this.author}, genre: ${this.genre}, has read: ${this.hasRead}`;
-    }
-}
 
 function addBookToLibrary() {
-  // do stuff here
+    let title = document.querySelector(".title").value;
+    let author = document.querySelector(".author").value;
+    let genre = document.querySelector(".genre").value;
+    let hasRead = document.querySelector(".has-read").checked;
+    let newBook = new Book(title, author, genre, hasRead);
+    myLibrary.push(newBook);
+    console.log(myLibrary);
+    bookcase.innerHTML = "";
+    displayBooks();
+    showForm();
+    return;
 }
 
 displayBooks();
 
-button.addEventListener('click', function(event) {
-    console.log("Form is clicked");
-    showForm();
-    event.preventDefault(); // Prevent form submission
-    
-});
